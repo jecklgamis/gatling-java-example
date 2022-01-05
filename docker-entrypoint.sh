492494d4f48ce8148f85fe57caf7e31b4074de82
Jerrico Gamis <jecklgamis@gmail.com>
2022-01-06 07:33:51 +1100

    Fix default simulation in Docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-SIMULATION_NAME=${SIMULATION_NAME:-gatling.test.example.simulation.ExampleGetSimulation}
+SIMULATION_NAME=${SIMULATION_NAME:-gatling.test.example.simulation.ExampleSimulation}
 exec java ${JAVA_OPTS} -cp bin/gatling-java-example.jar  io.gatling.app.Gatling -s ${SIMULATION_NAME}
