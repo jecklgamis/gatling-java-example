4ce64376fe956d780c391b0a70a135f8127eef03
Jerrico Gamis <jecklgamis@gmail.com>
2023-06-24 06:24:24 +1000

    Refactor SystemPropertiesUtil

--- src/main/java/gatling/test/example/simulation/SystemPropertiesUtil.java
+++ src/main/java/gatling/test/example/simulation/SystemPropertiesUtil.java
@@ -2,43 +2,43 @@ package gatling.test.example.simulation;
 
 public final class SystemPropertiesUtil {
     public static String getAsStringOrElse(String key, String fallback) {
-        Object value = System.getProperty(key);
+        String value = System.getProperty(key);
         if (value == null) {
             return fallback;
         }
-        return (String) value;
+        return value;
     }
 
     public static double getAsDoubleOrElse(String key, double fallback) {
-        Object value = System.getProperty(key);
+        String value = System.getProperty(key);
         if (value == null) {
             return fallback;
         }
-        return Double.parseDouble((String) value);
+        return Double.parseDouble(value);
     }
 
     public static int getAsIntOrElse(String key, int fallback) {
-        Object value = System.getProperty(key);
+        String value = System.getProperty(key);
         if (value == null) {
             return fallback;
         }
-        return Integer.parseInt((String) value);
+        return Integer.parseInt(value);
     }
 
     public static long getAsLongOrElse(String key, long fallback) {
-        Object value = System.getProperty(key);
+        String value = System.getProperty(key);
         if (value == null) {
             return fallback;
         }
-        return Long.parseLong((String) value);
+        return Long.parseLong(value);
     }
 
 
     public static boolean getAsBooleanOrElse(String key, boolean fallback) {
-        Object value = System.getProperty(key);
+        String value = System.getProperty(key);
         if (value == null) {
             return fallback;
         }
-        return Boolean.parseBoolean((String) value);
+        return Boolean.parseBoolean(value);
     }
 }
